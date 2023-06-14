96825ed3d2a26940ae8682c03c17d49ad179f9fb
SudhanshuRoy <shudhanshukumarmuz@gmail.com>
2023-06-15 02:02:17 +0530

    initial commit

---
+++ amazon-clone/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import NavBar from './Components/NavBar/Navigation';
+import MainPage from './Components/HomePage/MainPage';
+
+function App() {
+  return (
+    <div className="App">
+     <NavBar/>
+     <MainPage/>
+    </div>
+  );
+}
+
+export default App;
---
+++ amazon-clone/src/Components/HomePage/MainPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class MainPage extends Component {
+    constructor(props) {
+        super(props);
+    }
+    state = {  }
+    render() { 
+        return ( <div>Home Page</div> );
+    }
+}
+ 
+export default MainPage;
